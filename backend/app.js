88e8383ec62e7a26ac22e4431eeb83aaa1b7d504
Mehran Kamal <mkbalochhoth@gmail.com>
2021-01-26 20:56:01 +0500

    Database connected and necessary middlewares added.

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const userRouter = require('./routes/api/Users');
 require('dotenv').config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Growgreen database connected'))
+  .catch(err => console.error(err))
+
+const userRouter = require('./routes/api/users');
+
+//Middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 //API routers
 app.use("/api/user", userRouter);
 
 app.get('/', (req, res) => {
-    res.send("Hello");
+  res.json({"hello": 'hello mehran'});
 })
 
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`Growgreen backend listening at ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Growgreen backend listening at ${PORT}`);
 })
--- backend/routes/api/Users.js
+++ backend/routes/api/Users.js
@@ -4,7 +4,7 @@ const  router = express.Router();
 
 
 router.get('/', (req, res) => {
-    
+    res.json({"hello": "hello"});
 })
 
 module.exports = router;
