85aae33209149eafe5d1e2cefb3dd13a9523f8c3
Mehran Kamal <mkbalochhoth@gmail.com>
2021-01-27 21:48:34 +0500

    image attribute added in watered.js for wateredSchema.

--- backend/models/watered.js
+++ backend/models/watered.js
@@ -14,6 +14,10 @@ const wateredSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
+    },
+    image: {
+        type: String,
+        required: true
     }
 });
 
