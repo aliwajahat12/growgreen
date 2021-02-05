18ca2022b57a7ba0a78b1ac69e3a523e5e8a80d8
Mehran Kamal <mkbalochhoth@gmail.com>
2021-02-05 18:55:56 +0500

    Patch leaderboard.

--- nodejs-server/controllers/core/credit.js
+++ nodejs-server/controllers/core/credit.js
@@ -49,14 +49,8 @@ module.exports = {
     try {
       const overallLeaderboard = await CreditModel.aggregate()
         .lookup({
-          from: PlantedModel.collection.name,
-          localField: "plantedId",
-          foreignField: "_id",
-          as: "plantedData",
-        })
-        .lookup({
           from: UserModel.collection.name,
-          localField: "plantedData.userId",
+          localField: "userId",
           foreignField: "_id",
           as: "userData",
         })
@@ -96,45 +90,39 @@ module.exports = {
           date: { $gte: datePriorWeek },
         })
         .lookup({
-          from: PlantedModel.collection.name,
-          localField: "plantedId",
-          foreignField: "_id",
-          as: "plantedData",
-        })
-        .lookup({
-          from: UserModel.collection.name,
-          localField: "plantedData.userId",
-          foreignField: "_id",
-          as: "userData",
-        })
-        .unwind({
-          path: "$userData",
-          preserveNullAndEmptyArrays: true,
-        })
-        .group({
-          _id: "$userData._id",
-          totalCredits: { $sum: "$credits" },
-        })
-        .lookup({
-          from: UserModel.collection.name,
-          localField: "_id",
-          foreignField: "_id",
-          as: "userData",
-        })
-        .unwind({
-          path: "$userData",
-          preserveNullAndEmptyArrays: true,
-        })
-        .project({
-          userData: { email: 0 },
-        })
-        .project({
-          userData: { passwd: 0 },
-        })
-        .project({
-          userData: { __v: 0 },
-        })
-        .sort("-totalCredits");
+            from: UserModel.collection.name,
+            localField: "userId",
+            foreignField: "_id",
+            as: "userData",
+          })
+          .unwind({
+            path: "$userData",
+            preserveNullAndEmptyArrays: true,
+          })
+          .group({
+            _id: "$userData._id",
+            totalCredits: { $sum: "$credits" },
+          })
+          .lookup({
+            from: UserModel.collection.name,
+            localField: "_id",
+            foreignField: "_id",
+            as: "userData",
+          })
+          .unwind({
+            path: "$userData",
+            preserveNullAndEmptyArrays: true,
+          })
+          .project({
+            userData: { email: 0 },
+          })
+          .project({
+            userData: { passwd: 0 },
+          })
+          .project({
+            userData: { __v: 0 },
+          })
+          .sort("-totalCredits");
       console.log(weeklyLeaderboard);
       res.json({ overallLeaderboard, weeklyLeaderboard });
     } catch (err) {
