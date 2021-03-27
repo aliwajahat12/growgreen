4e02b1e53341a2e6df6ca778f686b92ffbeb57a5
Mehran Kamal <mehran_kamal@outlook.com>
2021-03-28 03:12:54 +0500

    Detection route added. Route: GET $FLASK_APP/landcover/?params
    params:
    	lat: Latitude
    	long: longitude
    	user_id: User id

--- flask-app/detection_script.py
+++ flask-app/detection_script.py
@@ -1,53 +1,88 @@
 import tensorflow as tf
 import cv2
 import numpy as np
-from flask import Flask, request
-import json
+import requests
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config["DEBUG"] = True
+
+new_model = tf.keras.models.load_model('./leaves_detection.h5')
 
 def create_mask(pred_mask):
   pred_mask = tf.argmax(pred_mask, axis=-1)
   pred_mask = pred_mask[..., tf.newaxis]
   return pred_mask[0]
 
-new_model = tf.keras.models.load_model('./leaves_detection.h5')
-
-
-app = Flask(__name__)
-app.config["DEBUG"] = True
-
-
-@app.route('/detect/', methods=['POST'])
+@app.route('/landcover/', methods=['GET'])
 def detect():
-  img_path = request.get_json()['image-path']
-  img = cv2.imread(img_path)
+  lati, longi, user_id = request.args['lat'], request.args['long'], request.args['user_id']
+  img_name = f'{lati}-{longi}.jpg'
+
+  response = requests.get(f"https://maps.googleapis.com/maps/api/staticmap?center={lati},{longi}&zoom=21&size=512x512&format=jpg&maptype=satellite&key=AIzaSyCr0-s_qBQozzmLIAzQvnUWwRSQUMuhwN4")
+  print("API Call: ", f"https://maps.googleapis.com/maps/api/staticmap?center={lati},{longi}&zoom=21&size=512x512&format=jpg&maptype=satellite&key=AIzaSyCr0-s_qBQozzmLIAzQvnUWwRSQUMuhwN4")
+
+  with open(f"media/{img_name}", 'wb') as file:
+    file.write(response.content)
+
+  img = cv2.imread(f'media/{img_name}')
+
+  # img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  # (h,s,v) = cv2.split(img)
+  # s = s*2
+  # s = np.clip(s, 0, 255)
+  # img = cv2.merge([h,s,v])
+  # img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+
   img = np.resize(img, [1, 128,128, 3])
+
   pred_mask = new_model.predict(img)
   pred_mask = create_mask(pred_mask)
+
   p_m = pred_mask.numpy()
+
   mask_inv = cv2.bitwise_not(p_m)
 
-  image = cv2.imread(img_path)
+  image = cv2.imread(f'media/{img_name}')
+
+  # image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+  # (h,s,v) = cv2.split(image)
+  # s = s*2
+  # s = np.clip(s, 0, 255)
+  # image = cv2.merge([h,s,v])
+  # image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
   rows, cols, channels = image.shape
   image = image[0:rows, 0:cols]
+
   bigger = cv2.resize(p_m.reshape((128,128)).astype('uint8'), (512, 512))
+
   kernel = np.ones((5,5),np.uint8)
   erosion = cv2.erode(bigger,kernel,iterations = 1)
   bigger = cv2.dilate(erosion,kernel,iterations = 1)
-  bigger = cv2.dilate(erosion,kernel,iterations = 1)
+
+
   colored_portion = cv2.bitwise_and(image, image, mask = bigger)
   colored_portion = colored_portion[0:rows, 0:cols]
+
   gray_portion = cv2.bitwise_and(gray, gray, mask = bigger)
   gray_portion = np.stack((gray_portion,)*3, axis=-1)
+
   output = colored_portion + gray_portion
+  conditional = output.reshape(-1, 3).sum(axis=1) <= 3
+  summ = np.sum(conditional)/(512 * 512)
+  plantableFraction = (1 - summ) * 100
+  print("Plantable area:", (1 - summ) * 100, "%")
 
-  resulting_path = img_path['path'].split('.')[0] + "-detected.jpg"
-  cv2.imwrite(resulting_path, output)
-  
-  res = {}
-  res['res_path'] = resulting_path
+  cv2.imwrite('output2.jpg', output)
 
-  return json.dumps(res), 200, {"Content-Type": 'application/json'}
+  # emit("landCoverDone", {'lat': lat, "long": long, 'user_id': user_id, 'pred': pred})
 
-app.run()
+  return jsonify({"pred": plantableFraction})
 
+if __name__=='__main__':
+  app.run(host="*", port=5000)
